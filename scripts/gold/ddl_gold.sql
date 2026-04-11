0b9b1048b0dcd149ed46c1b2a1ebb0b80240d97f
rb2002 <54690558+rb2002@users.noreply.github.com>
2026-04-11 16:41:46 -0400

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -91,18 +91,4 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON		  ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON		  ci.cst_key = la.cid
-
-
-SELECT DISTINCT
-		ci.cst_gndr,
-		ca.gen,
-		CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
-			 ELSE COALESCE(ca.gen, 'n/a')
-		END AS new_gen
-FROM silver.crm_cust_info ci
-LEFT JOIN silver.erp_cust_az12 ca
-ON		  ci.cst_key = ca.cid
-LEFT JOIN silver.erp_loc_a101 la
-ON		  ci.cst_key = la.cid
-
-SELECT distinct gender FROM gold.dim_customers
+/*
